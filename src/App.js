598ab0bb98abe2f87031549f12a138bb893a5301
saimaar <rahman.saima92@gmail.com>
2020-11-02 20:01:32 -0500

    implementing hooks for state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {useState, useEffect } from 'react'
 import { withRouter } from 'react-router-dom'
 import './App.css';
 import { Route, Switch } from 'react-router'
@@ -9,127 +9,142 @@ import HeaderContainer from './HeaderContainer.jsx'
 import Footer from './Footer'
 import NotFound from './NotFound';
 
-class App extends Component {
+function App () {
 
-  state={
-    user: {},
-    destinations:[],
-    search: "",
-    error: ""
-  }
+    let [destinations, setDestinations] = useState([])
+    let [user, setUser] = useState({})
+    console.log(destinations);
 
-  componentDidMount() {
-    fetch(`https://traveladvisor-api.herokuapp.com/destinations`)
-    .then(r => r.json())
-    .then((destinations) => {
-      this.setState({
-        destinations
+    useEffect(() =>{
+      fetch(`https://traveladvisor-api.herokuapp.com/destinations`)
+      .then(resp => resp.json())
+      .then(destinations => {
+        setDestinations(destinations)
       })
-    })
 
-    if(localStorage.token){
-      console.log(localStorage);
-      fetch(`https://traveladvisor-api.herokuapp.com/persist`, {
-        headers: {
-          "Authorization": `bearer ${localStorage.token}`
-        }
-      })
-      .then(r => r.json())
-      .then((data) => {
-        console.log(data);
-        if(data.token){
-
-          this.setState({
-              user: data.user
-            })
-          }
-        })
-      }
-  }
-
-  createNewUser = (newUser) => {
-    this.setState({
-      user: newUser.user,
-      token: newUser.token
-    })
-  }
-
-  loginUser = (user) => {
-    fetch('https://traveladvisor-api.herokuapp.com/login', {
-      method: "POST",
-      headers: {
-        "content-type": "application/json"
-      },
-      body: JSON.stringify(user)
-    })
-    .then(r => r.json())
-    .then(userData => {
-      if(userData.error){
-        this.setState({
-          error: userData.error
-        })
-      }else{
-        localStorage.setItem("token", userData.token)
-        this.setState({
-          user: userData.user,
-          search: ""
-        })
-      }
-    })
+      if(localStorage.token){
+         console.log(localStorage);
+         fetch(`https://traveladvisor-api.herokuapp.com/persist`, {
+           headers: {
+             "Authorization": `bearer ${localStorage.token}`
+           }
+         })
+         .then(r => r.json())
+         .then((data) => {
+           console.log(data);
+           if(data.token){
+             setUser(data.user)
+             }
+           })
+         }
 
 
+    },[])
+console.log(user);
 
+  //
+  // state={
+  //   user: {},
+  //   destinations:[],
+  //   search: "",
+  //   error: ""
+  // }
 
-  }
+  // componentDidMount() {
+  //   fetch(`https://traveladvisor-api.herokuapp.com/destinations`)
+  //   .then(r => r.json())
+  //   .then((destinations) => {
+  //     this.setState({
+  //       destinations
+  //     })
+  //   })
+  //
+  //   if(localStorage.token){
+  //     console.log(localStorage);
+  //     fetch(`https://traveladvisor-api.herokuapp.com/persist`, {
+  //       headers: {
+  //         "Authorization": `bearer ${localStorage.token}`
+  //       }
+  //     })
+  //     .then(r => r.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //       if(data.token){
+  //
+  //         this.setState({
+  //             user: data.user
+  //           })
+  //         }
+  //       })
+  //     }
+  // }
 
-  updateSearchForm = (newValue) => {
-    this.setState(prevState => {
-      return {
-        search: newValue
-      }
-    })
-  }
-
-  filterSearch = () => {
-    let newArr = this.state.destinations.filter( destination => {
-      let searchValue = this.state.search.toLowerCase()
-      return destination.name.toLowerCase().includes(searchValue) || destination.trip_type.toLowerCase().includes(searchValue)
-    })
-    return newArr
-  }
-
-  clearSearch = (emptyValue) => {
-    this.setState({
-      search: emptyValue
-    })
-  }
-
-  render() {
-     console.log(this.state.user);
-    let destinationsId = this.state.destinations.map(destination => destination.id)
-
+  // createNewUser = (newUser) => {
+  //   this.setState({
+  //     user: newUser.user,
+  //     token: newUser.token
+  //   })
+  // }
+  //
+  // loginUser = (user) => {
+  //   fetch('https://traveladvisor-api.herokuapp.com/login', {
+  //     method: "POST",
+  //     headers: {
+  //       "content-type": "application/json"
+  //     },
+  //     body: JSON.stringify(user)
+  //   })
+  //   .then(r => r.json())
+  //   .then(userData => {
+  //     if(userData.error){
+  //       this.setState({
+  //         error: userData.error
+  //       })
+  //     }else{
+  //       localStorage.setItem("token", userData.token)
+  //       this.setState({
+  //         user: userData.user,
+  //         search: ""
+  //       })
+  //     }
+  //   })
+  //
+  //
+  //
+  //
+  // }
+  //
+  // updateSearchForm = (newValue) => {
+  //   this.setState(prevState => {
+  //     return {
+  //       search: newValue
+  //     }
+  //   })
+  // }
+  //
+  // filterSearch = () => {
+  //   let newArr = this.state.destinations.filter( destination => {
+  //     let searchValue = this.state.search.toLowerCase()
+  //     return destination.name.toLowerCase().includes(searchValue) || destination.trip_type.toLowerCase().includes(searchValue)
+  //   })
+  //   return newArr
+  // }
+  //
+  // clearSearch = (emptyValue) => {
+  //   this.setState({
+  //     search: emptyValue
+  //   })
+  // }
+  //
+  //
+  //    console.log(this.state.user);
+  //   let destinationsId = this.state.destinations.map(destination => destination.id)
+  //
     return (
       <div className="page-window">
-          <HeaderContainer error={this.state.error} createNewUser={this.createNewUser} loginUser={this.loginUser} historyProps={this.props} />
-          <hr className="header-separation"/>
 
-        <Switch>
-          <Route exact path='/' render={ () => <HomeContainer
-          clearSearch={this.clearSearch}
-          destinations={this.filterSearch()}
-          search={this.state.search}
-          updateSearchForm={this.updateSearchForm}
-          routerProps={this.props}
-          user={this.state.user}
-          />} />
-          <Route exact path='/profile' render={localStorage.token ? (routerProps) => <ProfileContainer routerProps={routerProps} /> : (routerProps) => <NotFound />}/>
-          <Route path='/:id' render={ (routerProps) => <ShowContainer user={this.state.user} routerProps={routerProps} destinationsId={destinationsId} /> }/>
-        </Switch>
-
-        <Footer />
       </div>
     )
-  }
 
 }
 
